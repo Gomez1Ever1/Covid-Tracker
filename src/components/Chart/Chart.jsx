3047c2cd470a37b90b47f217b77905abc94c19b3
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-05-14 21:12:53 -0700

    created repository, complete MVP app, may need some cleaning. app renders all intended data and toggles regions, no issue to date

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
+
+const Chart = (data: { confirmed, recovered, deaths }, country) => {
+    const [dailyData, setDailyData] = useState([]);
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        };
+        fetchAPI();
+    });
+    const barChart = confirmed ?
+        (
+            <Bar data={{
+                label: ["Infected", "Recovered", "Deaths"],
+                datasets: [{
+                    labels: "People",
+                    backgroundColor: [
+                        "rgba(0,0,255,0.5)",
+                        "rgba(0,255,0,0.5)",
+                        "rgba(255,0,0,0.5)"
+                    ],
+                    data: [confirmed, recovered, deaths]
+                }]
+            }} option={{
+                legend: { display: false },
+                title: { display: true, text: "Current state in" + country }
+            }
+            }></Bar>
+        ) : null
+    );
+const lineChart = (
+    dailyData.length ? (
+        <Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [{
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true
+                }, {
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColr: 'rgba(255,0,0,0.5)',
+                    fill: true
+                }]
+            }} />) : null
+);
+return (
+    <div className={styles.container}>
+        {lineChart}
+    </div>
+)
+};
+export default Chart; 
